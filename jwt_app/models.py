87e97db8f3b3806080031e91bc96a0b35bca8984
Abi James <a.james1@live.co.uk>
2020-04-10 12:25:21 +0100

    update

--- jwt_app/models.py
+++ jwt_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import ArrayField
+from django.apps import apps
 
 Chatroom = apps.get_model('food_swap', 'Chatroom')
 
