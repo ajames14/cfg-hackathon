bf212e1d048ebbd83cb76fb90ac8ac15d98fde71
Kathrin Eichinger <kathrin801@gmail.com>
2020-04-13 12:26:50 +0100

    fix str chatroom model

--- food_swap/models.py
+++ food_swap/models.py
@@ -9,7 +9,7 @@ class Chatroom(models.Model):
   postcode = models.CharField(max_length=4, unique=True)
   # users = ArrayField(models.ForeignKey(User, related_name='chatroom', on_delete=models.PROTECT, null=True))
 
-def __str__(self):
+  def __str__(self):
     return self.postcode
 
 
