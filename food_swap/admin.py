0d698d8002cf8d3b06e8dccc39b2950657ea04fe
Kathrin Eichinger <kathrin801@gmail.com>
2020-04-13 10:19:11 +0100

    registered food_swap models in admin site

--- food_swap/admin.py
+++ food_swap/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Chatroom, Post, Comment
 
 # Register your models here.
+admin.site.register(Chatroom)
+admin.site.register(Post)
+admin.site.register(Comment)
