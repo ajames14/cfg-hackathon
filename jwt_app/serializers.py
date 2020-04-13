58ac4d8c41e7d0ca62148a33c7651e3c3c15366d
Kathrin Eichinger <kathrin801@gmail.com>
2020-04-13 12:11:21 +0100

    fix userserializer

--- jwt_app/serializers.py
+++ jwt_app/serializers.py
@@ -33,4 +33,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password', 'postcode', 'favourites')
