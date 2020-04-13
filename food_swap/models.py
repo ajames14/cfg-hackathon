79dfce4c57f05456cd5e67b36b6e206a8adbd17e
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-04-13 14:53:10 +0100

    chatroom user relation added

--- food_swap/models.py
+++ food_swap/models.py
@@ -6,28 +6,34 @@ User = get_user_model()
 # Create your models here.
 
 class Chatroom(models.Model):
-  postcode = models.CharField(max_length=4, unique=True)
-  # users = ArrayField(models.ForeignKey(User, related_name='chatroom', on_delete=models.PROTECT, null=True))
+    postcode = models.CharField(max_length=4, unique=True)
+    users = models.ManyToManyField(User, related_name='chatroom', blank=True)
 
-  def __str__(self):
-    return self.postcode
+    def __str__(self):
+        return self.postcode
+
+# alternative option to ManyToMany Field:
+class Intermediary(models.Model):
+    user = models.ForeignKey(User, related_name='medium', on_delete=models.CASCADE)
+    Chatroom = models.ForeignKey(Chatroom, related_name='medium', on_delete=models.CASCADE)
 
 
 class Post(models.Model):
-  user = models.ForeignKey(User, related_name='post', on_delete=models.CASCADE)
-  chatroom = models.ForeignKey(Chatroom, related_name='chatroom', on_delete=models.CASCADE)
-  text = models.CharField(max_length=2000)
-  time_stamp = models.DateTimeField(auto_now_add=True, null=True)
-  is_swapped = models.BooleanField(default=False)
+    user = models.ForeignKey(User, related_name='post', on_delete=models.CASCADE)
+    chatroom = models.ForeignKey(Chatroom, related_name='chatroom', on_delete=models.CASCADE)
+    text = models.CharField(max_length=2000)
+    time_stamp = models.DateTimeField(auto_now_add=True, null=True)
+    is_swapped = models.BooleanField(default=False)
 
-  def __str__(self):
-    return self.user
+    def __str__(self):
+        return self.user
+
 
 class Comment(models.Model):
-  user = models.ForeignKey(User, related_name='comment', on_delete=models.CASCADE)
-  post = models.ForeignKey(Post, related_name='comment', on_delete=models.CASCADE)
-  text = models.CharField(max_length=2000)
-  time_stamp = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User, related_name='comment', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comment', on_delete=models.CASCADE)
+    text = models.CharField(max_length=2000)
+    time_stamp = models.DateTimeField(auto_now_add=True, null=True)
 
-  def __str__(self):
-    return self.user
+    def __str__(self):
+        return self.user
