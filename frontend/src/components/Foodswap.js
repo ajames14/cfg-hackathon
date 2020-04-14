19fe322364cc12b15b7233aaf2ad4eaf8897ec34
marepstein <marissaepstein@outlook.com>
2020-04-14 17:30:26 +0100

    frontend sass

---
+++ frontend/src/components/Foodswap.js
@@ -0,0 +1,11 @@
+/************
+ * 
+ * METHOD:
+ * if statement or '? :' line of code that checks if userInfo.postcode !== null  
+ * if null --> show form to enter postcode --> PUT request to user profile 
+ * if userInfo = null, then show a register form 
+ * this will then refresh/update userInfo (if possible without refreshing the page)
+ * if postcode exists show <Chatroom /> - similar logic to buttons in my side project
+ * in chatroom component, the check to match user postcode to chat postcode will be here 
+ 
+ ***********/
