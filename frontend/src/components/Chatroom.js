ad2991c060d23c4e683bb5f17447931ad2ccfc67
marepstein <marissaepstein@outlook.com>
2020-04-16 17:45:20 +0100

    food swap

--- frontend/src/components/Chatroom.js
+++ frontend/src/components/Chatroom.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
+/*******
+ * Shows number of users or user icons
+ * GET users
+ * GET posts
+ * GET comments
+ * Map through to get all posts
+ * If no posts or user == 1 show text saying something like 'welcome, be the first to post'
+ * *******/
+
 const Chatroom = () => {
   return (
     <div className="section">
