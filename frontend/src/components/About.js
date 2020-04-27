bf6a481cfce3681dd45a22c5872b00ab35a83d74
marepstein <marissaepstein@outlook.com>
2020-04-27 15:54:08 +0100

    about

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,44 +1,30 @@
 import React, { useRef, useEffect, useState } from 'react'
-import { useSpring, animated } from 'react-spring'
+import Fade from 'react-reveal/Fade'
 
-import Title from './Title'
 import AboutChallenges from './Challenges'
 import AboutContent from './Concept'
 
 const About = () => {
-  const [currentSlide, setCurrentSlide] = useState(AboutChallenges)
-  const [visibleSlide2, showVisibleSlide2] = useState(false)
+  const [slide1, showSlide1] = useState(true)
+  const [slide2, showSlide2] = useState(false)
 
-  const slide1 = AboutChallenges
-  const slide2 = AboutContent
-  const Components = [AboutChallenges, AboutContent]
-
-  function nextSlide() {
-    setCurrentSlide(slide2)
-    showVisibleSlide2(true)
-    console.log(currentSlide)
-    console.log(showVisibleSlide2)
+  function changeSlide() {
+    showSlide1(!slide1)
+    showSlide2(!slide2)
   }
-
-  function prevSlide() {
-    setCurrentSlide(slide1)
-    console.log(currentSlide)
-  }
-
-  console.log(currentSlide)
 
   return (
     <div className="section" id="about-section">
-      <div className="container">
-        {/* {Components.map((Component, i) => {
-          return <Component key={i} />
-        })} */}
-        {currentSlide === slide2 ? <AboutContent /> : <AboutChallenges />}
-        <button onClick={nextSlide}>More</button>
-        <button onClick={prevSlide}>Back</button>
+      <div className="container is-half">
+        {slide1 && <AboutChallenges />}
+        {slide2 && <AboutContent />}
+        <Fade bottom delay={1500}>
+          <div className="button" onClick={changeSlide}>
+            {slide1 ? 'More' : 'Back'}
+          </div>
+        </Fade>
       </div>
     </div>
   )
 }
-
 export default About
