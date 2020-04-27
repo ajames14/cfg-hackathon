b8381f01c496df4d48c0a4c17de3bb974c1d8bc9
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-04-27 17:42:36 +0100

    home merge fix

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,39 +7,24 @@ const Home = () => {
   return (
     <div className="section" id="home-section">
       <div className="container">
-<<<<<<< HEAD
-        <div className="box">
-          <div className="title">HELPING YOU CUT CROWDS & MINIMIZE WASTE</div>
-          <Link to="/about" className="small-link">
-            Find out more...
-          </Link>
-        </div>
-=======
         <Link to="/about">
-          <div className="box hvr-shrink" id="lrg-box">
-            <div className="home-link" id="large-link">
+          <div className="box hvr-shrink">
+            <div className="home-link">
               HELPING YOU CUT CROWDS & MINIMIZE WASTE
             </div>
-            <div style={{ margin: '40px 0px 30px 0px' }}>
-              <div
-                to="/about"
-                className="small-link"
-                style={{ paddingLeft: '8px', color: 'white' }}
-              >
-                Find out more...
-              </div>
+            <div to="/about" className="small-link">
+              Find out more...
             </div>
           </div>
         </Link>
->>>>>>> development
       </div>
       <div className="container" id="small-container">
-        <Link to='/recipes'>
+        <Link to="/recipes">
           <div className="box hvr-shrink" id="small-box">
             <div className="home-link">FIND YOUR RECIPES</div>
           </div>
         </Link>
-        <Link to='/foodswap'>
+        <Link to="/foodswap">
           <div className="box hvr-shrink" id="small-box">
             <div className="home-link">SHARE YOUR FOOD</div>
           </div>
@@ -48,7 +33,4 @@ const Home = () => {
     </div>
   )
 }
-
 export default Home
-
-// CONVERT THIS TO CSS GRID?
