a04ea0e4fba41f3a7f33aa67fb77500cdbb57a91
Kathrin Eichinger <kathrin801@gmail.com>
2020-05-01 18:44:28 +0100

    ready for deployment

--- project/settings.py
+++ project/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
 import django_heroku # put this at the top of the file
+import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
